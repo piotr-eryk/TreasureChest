d84c4abab036e4d1ca826dd72391c80348b60bc1
Piotr <piter1997_21@wp.pl>
2023-10-28 17:24:16 +0200

    add grabbing mechanic

--- Assets/Scripts/GrabbableObject.cs
+++ Assets/Scripts/GrabbableObject.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class GrabbableObject : MonoBehaviour, IGrabbable
 {
-    [SerializeField]
-    private float grabbingDistance = 2f;
-
     public void GrabObject()
     {
         gameObject.SetActive(false);//back to pool
--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
 {
+    [SerializeField]
+    private float grabbingDistance = 2f;
+    [SerializeField]
+    private Camera playerCamera;
+
+    private GrabbableObject grabbableObject;
+
     public void TakeObject()
     {
+        if (Physics.Raycast(
+            transform.position,
+            playerCamera.transform.forward,
+            out RaycastHit hit,
+            grabbingDistance) && hit.transform.GetComponent<GrabbableObject>())
+
+        {
+            grabbableObject = hit.transform.GetComponent<GrabbableObject>();
+            grabbableObject?.GrabObject();
+        }
 
     }
 }
